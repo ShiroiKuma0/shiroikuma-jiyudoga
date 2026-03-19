3c224f68cdda8af5df2f14e9d9830822c597b897
Emma <MarmadileManteater@proton.me>
2026-03-19 11:28:15 -0400

    Update webpack.android.config for shaka update

--- _scripts/webpack.android.config.js
+++ _scripts/webpack.android.config.js
@@ -175,9 +175,9 @@ const config = {
     alias: {
 
       DB_HANDLERS_ELECTRON_RENDERER_OR_WEB$: path.resolve(__dirname, '../src/datastores/handlers/web.js'),
-
-      // change to "shaka-player.ui.debug.js" to get debug logs (update jsconfig to get updated types)
-      'shaka-player$': 'shaka-player/dist/shaka-player.ui.js'
+      
+      // change to "shaka-player.ui-es2021.debug.js" to get debug logs (update jsconfig to get updated types)
+      'shaka-player$': 'shaka-player/dist/shaka-player.ui-es2021.js',
     },
     fallback: {
       'fs/promises': path.resolve(__dirname, '_empty.js')
