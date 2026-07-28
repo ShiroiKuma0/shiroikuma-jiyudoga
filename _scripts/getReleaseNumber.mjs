ac04505f04f067549a09a00320680bf2845565ed
Emma Manteater <marmadilemanteater@proton.me>
2026-07-28 16:13:47 -0400

    keep going

--- _scripts/getReleaseNumber.mjs
+++ _scripts/getReleaseNumber.mjs
@@ -3,17 +3,3 @@ import packageJSON from '../package.json' with { type: 'json' }
 
 const latestRelease = await (await fetch('https://api.github.com/repos/MarmadileManteater/FreetubeAndroid/releases?per_page=1'))
 console.log(latestRelease)
-const code = latestRelease[0].name
-
-const [_, latestMajor, latestMinor, latestBuild] = code.split('.')
-
-const [__, major, minor] = packageJSON.version.split('.')
-
-if (
-  latestMajor == major &&
-  latestMinor == minor
-) {
-  console.log(parseInt(latestBuild) + 1)
-} else {
-  console.log(1)
-}
