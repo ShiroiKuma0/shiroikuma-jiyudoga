8bcb577923495e6e5d049e626023d70afa1336ce
Emma Manteater <marmadilemanteater@proton.me>
2026-07-28 16:15:50 -0400

    try again

--- _scripts/getReleaseNumber.mjs
+++ _scripts/getReleaseNumber.mjs
@@ -1,5 +1,5 @@
 import { fetch } from 'undici'
 import packageJSON from '../package.json' with { type: 'json' }
 
-const latestRelease = await (await fetch('https://api.github.com/repos/MarmadileManteater/FreetubeAndroid/releases?per_page=1'))
+const latestRelease = await (await fetch('https://api.github.com/repos/MarmadileManteater/FreetubeAndroid/releases?per_page=1')).text()
 console.log(latestRelease)
